e19931f1be2070fd800d8dc2a6ffd3c7f961874c
Sameer Khan <sameerkhanaa5@gmail.com>
2026-09-26 11:42:55 +0530

    Challenge 4

--- 01-javaScript/phase-1.js
+++ 01-javaScript/phase-1.js
@@ -64,5 +64,68 @@ function palindromeChecker(word=""){
     }
 }
 
+
+
+// AI's Answer
+
+// function isPalindrome(text) {
+//     // Step 1: Regex ka use karke a-z aur 0-9 ke alawa sab kuch ('') empty string se replace kar do
+//     // /[^a-z0-9]/gi ka matlab: g = global (puri string mein), i = case insensitive
+//     const cleaned = text.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    
+//     // Step 2: Two Pointers
+//     let left = 0;
+//     let right = cleaned.length - 1;
+    
+//     while (left < right) {
+//         if (cleaned[left] !== cleaned[right]) {
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+    
+//     return true;
+// }
+
 // console.log(palindromeChecker("pat t ap"))
 // console.log(palindromeChecker("No lemon, no melon"))
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Challenge 4 (Factorial - Loop vs Recursion)
+
+
+// Loop
+function factorial(n){
+    sum = 1
+
+    for(let i = 1; i < n; i++){
+        // console.log(`${sum} * ${i}`)
+        sum += sum * i
+    }
+    return sum
+}
+
+function factorialRecursive(n) {
+    if (n < 0) return "Invalid input";
+    
+    // BASE CASE
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    
+    // RECURSIVE CALL
+    return n * factorialRecursive(n - 1);
+}
+
+// console.log(factorial(1))
+// console.log(factorial(2))
+// console.log(factorial(3))
+// console.log(factorial(4))
+// console.log(factorial(5))
+// console.log(factorial(10))
